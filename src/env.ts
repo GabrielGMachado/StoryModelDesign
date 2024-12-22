e29e72b0ea65beb64cc7ce6279845c74d4d8d61b
Gabriel Machado <bielgaelzermachado@gmail.com>
2024-12-21 22:20:10 -0300

    featured-products

--- src/env.ts
+++ src/env.ts
@@ -1,15 +1,18 @@
-import { z } from "zod"
+import { z } from 'zod'
 
-const envScheme = z.object({
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 })
 
-const parsedEnv = envScheme.safeParse(process.env)
+const parsedEnv = envSchema.safeParse(process.env)
 
 if (!parsedEnv.success) {
-    console.error('invalid environment variables', parsedEnv.error.flatten().fieldErrors)
+  console.error(
+    'Invalid environment variables',
+    parsedEnv.error.flatten().fieldErrors,
+  )
 
-    throw new Error('invalid environment variables.')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = parsedEnv.data
